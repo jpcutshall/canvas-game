72bdca5b0f4bdcf2c639f9ef36149c3a4052871c
jpcutshall <jpcutshall@gmail.com>
2021-06-25 02:06:56 -0400

    got balls bouncing around

---
+++ public/colors.js
@@ -0,0 +1,10 @@
+const colors = [
+    '#ebeb34',
+    '#34eb5e',
+    '#3458eb',
+    '#ab34eb',
+    '#eb3434',
+    '#82eaff'
+]
+
+export default colors;
--- public/script.js
+++ public/script.js
@@ -1,39 +1,74 @@
+import colors from './colors.js';
 const canvas = document.getElementById('canvas')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d')
 let sWidth = ctx.canvas.width;
 let sHeight = ctx.canvas.height;
 
-console.log((sWidth / 2))
+console.log(colors.length)
 
-ctx.fillStyle = 'green';
-// let triangle = new Path2D();
-// triangle.moveTo((sWidth / 2)+10, (sHeight / 2))
-// triangle.lineTo((sWidth / 2), (sHeight / 2)-20)
-// triangle.lineTo((sWidth / 2)-10, (sHeight / 2))
-// ctx.fill(triangle)
 
-class Player {
-    constructor(){
-        this.triangle = new Path2D();
-        this.triangle.moveTo((sWidth / 2)+10, (sHeight / 2))
-        this.triangle.lineTo((sWidth / 2), (sHeight / 2)-20)
-        this.triangle.lineTo((sWidth / 2)-10, (sHeight / 2))
-        ctx.fill(this.triangle)
-                      
+class Circle {
+    constructor (x, y, dx, dy, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius;
+        this.color = color
     }
 
+    draw () {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill()
+        
+    }
 
+    update () {
+
+        if (this.x + this.radius > sWidth || this.x - this.radius < 0) {
+        this.dx = -this.dx
+        }
+
+        if (this.y + this.radius > sHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy
+        }
+
+        this.x += this.dx
+        this.y += this.dy
+        this.draw()
+    }
+
+}
+
+const circleArray = []
+
+for (let i = 0; i < 1000; i++) {
+    let radius = 3
+    let randX = (Math.random() * (window.innerWidth - radius * 2) + radius)
+    let randY = (Math.random() * (window.innerHeight - radius * 2) + radius)
+    let randDY = (Math.random() - 0.5) * 6
+    let randDX = (Math.random() - 0.5) * 6
+    let randColor = colors[Math.floor(Math.random() * (colors.length - 1))]
+
+    circleArray.push(new Circle(randX, randY, randDX, randDY, radius, randColor))        
+}
+
+let bircle = new Circle(200, 200, 5, -5, 20)
+
+const animate = () => {
+    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
+    
+    for (let i = 0; i < circleArray.length; i++) {
+        circleArray[i].update() 
+    }
+    
 
     
 }
-let player = new Player()
 
-function init(){
-    
-    setInterval(draw, 100)
-}
-
-
-function draw(){
-    player.triangle.transform(1, 0, 0, 1, 1, 1)
-}
+animate()
