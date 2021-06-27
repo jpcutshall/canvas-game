ef7db7b5af21c95e3ef630fc21a54f4d98167772
jpcutshall <jpcutshall@gmail.com>
2021-06-26 20:21:00 -0400

    bouncing colored balls

--- docs/script.js
+++ docs/script.js
@@ -14,48 +14,48 @@ let mouseY = 0
 canvas.addEventListener('mousemove', (e) => {
     mouseX = e.offsetX;
     mouseY = e.offsetY;
-    console.log(mouseX, mouseX)
+    console.log(mouseX, mouseY)
 })
 
-class Triangle {
-    constructor () {
-        //this.angle = Math.asin(.5 * 59 /  30);
-        this.center = {x: cx, y: cy}
-        this.x1 = cx;
-        this.y1 = this.center.y - 15 ;
-        this.x2 = this.center.x - 15;
-        this.y2 = this.center.y + 30;
-        this.x3 = this.center.x + 15;
-        this.y3 = this.center.y + 30;
+// class Triangle {
+//     constructor () {
+//         //this.angle = Math.asin(.5 * 59 /  30);
+//         this.center = {x: cx, y: cy}
+//         this.x1 = cx;
+//         this.y1 = this.center.y - 15 ;
+//         this.x2 = this.center.x - 15;
+//         this.y2 = this.center.y + 30;
+//         this.x3 = this.center.x + 15;
+//         this.y3 = this.center.y + 30;
        
-    }
+//     }
 
-    // -10px from mouse pos
-    draw () {
-        ctx.strokeStyle = 'red'
-        ctx.beginPath()
-        ctx.moveTo(this.x1, this.y1)
-        ctx.lineTo(this.x2, this.y2)
-        ctx.lineTo(this.x3, this.y3)
-        ctx.closePath()
-        ctx.stroke()
-    }
+//     // -10px from mouse pos
+//     draw () {
+//         ctx.strokeStyle = 'red'
+//         ctx.beginPath()
+//         ctx.moveTo(this.x1, this.y1)
+//         ctx.lineTo(this.x2, this.y2)
+//         ctx.lineTo(this.x3, this.y3)
+//         ctx.closePath()
+//         ctx.stroke()
+//     }
 
-    update () {
+//     update () {
 
-        this.center.x = mouseX;
-        this.center.y = mouseY;
-        this.x1 = this.center.x;
-        this.y1 = this.center.y - 15 ;
-        this.x2 = this.center.x - 15;
-        this.y2 = this.center.y + 30;
-        this.x3 = this.center.x + 15;
-        this.y3 = this.center.y + 30;
+//         this.center.x = mouseX;
+//         this.center.y = mouseY - 5;
+//         this.x1 = this.center.x;
+//         this.y1 = this.center.y - 15 ;
+//         this.x2 = this.center.x - 15;
+//         this.y2 = this.center.y + 30;
+//         this.x3 = this.center.x + 15;
+//         this.y3 = this.center.y + 30;
 
 
-        this.draw()
-    }
-}
+//         this.draw()
+//     }
+// }
 
 
 
@@ -69,7 +69,7 @@ class Circle {
         this.color = color
     }
 
-    draw () {
+    draw() {
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.fillStyle = this.color;
@@ -77,7 +77,15 @@ class Circle {
         
     }
 
-    update () {
+    // checkMouse() {
+    //     let distance = Math.hypot((mouseX - this.x), (mouseY - this.y))
+    //     if (distance < 30) {
+    //         this.dx = -this.dx
+    //         this.dy = -this.dy
+    //     }
+    // }
+
+    update() {
 
         if (this.x + this.radius > sWidth || this.x - this.radius < 0) {
             this.dx = -this.dx
@@ -86,6 +94,7 @@ class Circle {
         if (this.y + this.radius > sHeight || this.y - this.radius < 0) {
             this.dy = -this.dy
         }
+        //this.checkMouse()
 
         this.x += this.dx
         this.y += this.dy
@@ -108,16 +117,17 @@ for (let i = 0; i < 1000; i++) {
     circleArray.push(new Circle(randX, randY, randDX, randDY, radius, randColor))        
 }
 
-let tri = new Triangle()
+
 
 const animate = () => {
     requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.2)'
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
     
     for (let i = 0; i < circleArray.length; i++) {
         circleArray[i].update() 
     }
-    tri.update()
+
 
     
 }
