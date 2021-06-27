21e23e8eebe41964c8a235b67e22fcdeb1055a39
jpcutshall <jpcutshall@gmail.com>
2021-06-26 23:08:26 -0400

    acceleration is down and friction. now for auntonomy

--- docs/script.js
+++ docs/script.js
@@ -8,14 +8,175 @@ let sHeight = ctx.canvas.height;
 let cx = sWidth / 2
 let cy = sHeight / 2
 
-let mouseX = 0
-let mouseY = 0
 
-canvas.addEventListener('mousemove', (e) => {
-    mouseX = e.offsetX;
-    mouseY = e.offsetY;
-    console.log(mouseX, mouseY)
-})
+
+class Circle {
+
+    constructor (x, y, vector, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.vector = {
+            x: vector.x,
+            y: vector.y
+        };
+        this.accel = {
+            x: 0,
+            y: 0
+        }
+        this.radius = radius;
+        this.color = color;
+        this.speedLimit = 10;
+        this.fric = 0.97;
+    }
+
+    draw() {
+
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill()
+        
+    }
+
+
+    gravity() {
+        if(this.vector.y < 9.8){
+            this.vector.y += 0.05 ;
+        }
+    }
+
+    checkWalls() {
+
+        if (this.x - this.radius < 0) {
+            this.vector.x = 0
+            this.x = this.radius
+        }
+        if (this.x + this.radius > sWidth) {
+            this.vector.x = 0
+            this.x = sWidth - this.radius
+        }
+
+        if (this.y - this.radius < 0) {
+            this.vector.y = 0
+            this.y = this.radius
+        }
+        if (this.y + this.radius > sHeight) {
+            this.vector.y = 0
+            this.y = sHeight - this.radius
+        }
+    }
+
+
+    velocityChecks() {
+        if (this.vector.x < this.speedLimit) {
+
+            this.vector.x += this.accel.x   
+        }
+
+        if (this.vector.y < this.speedLimit) {
+
+            this.vector.y = this.vector.y + this.accel.y   
+        } 
+        if (Math.abs(this.vector.x) < .03){
+
+            this.vector.x = 0
+        }
+        if (Math.abs(this.vector.y) < .03){
+
+            this.vector.y = 0
+        }
+
+        this.checkWalls()
+        this.vector.y *= this.fric
+        this.vector.x *= this.fric
+    }
+
+    
+    update() {
+
+        this.velocityChecks()
+
+        this.x += this.vector.x
+        this.y += this.vector.y
+        
+        this.draw()
+    }
+
+    
+}
+
+const onKeyPress = (e) => {
+    e.preventDefault()
+    if(e.keyCode === 39) {
+        circle.accel.x = .3
+        
+    }
+    if(e.keyCode === 37) {
+        circle.accel.x = -.3
+    }
+    if(e.keyCode == 38) {
+        circle.accel.y = -.3
+    }
+    if(e.keyCode == 40) {
+        circle.accel.y = .3
+    }
+}
+
+const onKeyUp = (e) => {
+    e.preventDefault()
+    if(e.keyCode == 39) {
+        circle.accel.x -= .3
+    }
+    if(e.keyCode == 37) {
+        circle.accel.x += .3
+    }
+    if(e.keyCode == 38) {
+        circle.accel.y += .3
+    }
+    if(e.keyCode == 40) {
+        circle.accel.y -= .3
+    }
+}
+
+
+let circle = new Circle(cx, cy, { x: 0, y: 0 }, 5, 'yellow')
+
+const animate = () => {
+
+    requestAnimationFrame(animate)
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.2)'
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
+
+    window.onkeydown = onKeyPress
+    window.onkeyup = onKeyUp
+    
+    circle.update()
+
+    
+}
+
+animate()
+
+
+
+
+
+
+
+
+//const circleArray = []
+
+// for (let i = 0; i < 1000; i++) {
+//     let radius = 3
+//     let randX = (Math.random() * (window.innerWidth - radius * 2) + radius)
+//     let randY = (Math.random() * (window.innerHeight - radius * 2) + radius)
+//     let randDY = (Math.random() - 0.5) * 6
+//     let randDX = (Math.random() - 0.5) * 6
+//     let randColor = colors[Math.floor(Math.random() * (colors.length - 1))]
+
+//     circleArray.push(new Circle(randX, randY, randDX, randDY, radius, randColor))        
+// }
+
 
 // class Triangle {
 //     constructor () {
@@ -56,80 +217,3 @@ canvas.addEventListener('mousemove', (e) => {
 //         this.draw()
 //     }
 // }
-
-
-
-class Circle {
-    constructor (x, y, dx, dy, radius, color) {
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
-        this.radius = radius;
-        this.color = color
-    }
-
-    draw() {
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        ctx.fillStyle = this.color;
-        ctx.fill()
-        
-    }
-
-    // checkMouse() {
-    //     let distance = Math.hypot((mouseX - this.x), (mouseY - this.y))
-    //     if (distance < 30) {
-    //         this.dx = -this.dx
-    //         this.dy = -this.dy
-    //     }
-    // }
-
-    update() {
-
-        if (this.x + this.radius > sWidth || this.x - this.radius < 0) {
-            this.dx = -this.dx
-        }
-
-        if (this.y + this.radius > sHeight || this.y - this.radius < 0) {
-            this.dy = -this.dy
-        }
-        //this.checkMouse()
-
-        this.x += this.dx
-        this.y += this.dy
-        this.draw()
-    }
-
-    
-}
-
-const circleArray = []
-
-for (let i = 0; i < 1000; i++) {
-    let radius = 3
-    let randX = (Math.random() * (window.innerWidth - radius * 2) + radius)
-    let randY = (Math.random() * (window.innerHeight - radius * 2) + radius)
-    let randDY = (Math.random() - 0.5) * 6
-    let randDX = (Math.random() - 0.5) * 6
-    let randColor = colors[Math.floor(Math.random() * (colors.length - 1))]
-
-    circleArray.push(new Circle(randX, randY, randDX, randDY, radius, randColor))        
-}
-
-
-
-const animate = () => {
-    requestAnimationFrame(animate)
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.2)'
-    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
-    
-    for (let i = 0; i < circleArray.length; i++) {
-        circleArray[i].update() 
-    }
-
-
-    
-}
-
-animate()
