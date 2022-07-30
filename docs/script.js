a8d32ce9953c862d17c46eada6df89cb590e3957
jpcut <jpcutshall@gmail.com>
2022-07-30 12:49:32 -0400

    formatted script. switching back to main to further expand this

--- docs/script.js
+++ docs/script.js
@@ -1,28 +1,24 @@
-import colors from './colors.js';
-const canvas = document.getElementById('canvas')
+const canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext('2d');
 let sWidth = ctx.canvas.width;
 let sHeight = ctx.canvas.height;
-let cx = sWidth / 2
-let cy = sHeight / 2
-
-
+let cx = sWidth / 2;
+let cy = sHeight / 2;
 
 class Circle {
-
-    constructor (x, y, vector, radius, color) {
+    constructor(x, y, vector, radius, color) {
         this.x = x;
         this.y = y;
         this.vector = {
             x: vector.x,
-            y: vector.y
+            y: vector.y,
         };
         this.accel = {
             x: 0,
-            y: 0
-        }
+            y: 0,
+        };
         this.radius = radius;
         this.color = color;
         this.speedLimit = 10;
@@ -30,139 +26,115 @@ class Circle {
     }
 
     draw() {
-
-        ctx.beginPath()
+        ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.fillStyle = this.color;
-        ctx.fill()
-        
+        ctx.fill();
     }
 
-
     gravity() {
-        if(this.vector.y < 9.8){
-            this.vector.y += 0.05 ;
+        if (this.vector.y < 9.8) {
+            this.vector.y += 0.05;
         }
     }
 
     checkWalls() {
-
         if (this.x - this.radius < 0) {
-            this.vector.x = 0
-            this.x = this.radius
+            this.vector.x = 0;
+            this.x = this.radius;
         }
         if (this.x + this.radius > sWidth) {
-            this.vector.x = 0
-            this.x = sWidth - this.radius
+            this.vector.x = 0;
+            this.x = sWidth - this.radius;
         }
 
         if (this.y - this.radius < 0) {
-            this.vector.y = 0
-            this.y = this.radius
+            this.vector.y = 0;
+            this.y = this.radius;
         }
         if (this.y + this.radius > sHeight) {
-            this.vector.y = 0
-            this.y = sHeight - this.radius
+            this.vector.y = 0;
+            this.y = sHeight - this.radius;
         }
     }
-
 
     velocityChecks() {
         if (this.vector.x < this.speedLimit) {
-
-            this.vector.x += this.accel.x   
+            this.vector.x += this.accel.x;
         }
 
         if (this.vector.y < this.speedLimit) {
-
-            this.vector.y = this.vector.y + this.accel.y   
-        } 
-        if (Math.abs(this.vector.x) < .03){
-
-            this.vector.x = 0
+            this.vector.y = this.vector.y + this.accel.y;
         }
-        if (Math.abs(this.vector.y) < .03){
-
-            this.vector.y = 0
+        if (Math.abs(this.vector.x) < 0.03) {
+            this.vector.x = 0;
+        }
+        if (Math.abs(this.vector.y) < 0.03) {
+            this.vector.y = 0;
         }
 
-        this.checkWalls()
-        this.vector.y *= this.fric
-        this.vector.x *= this.fric
+        this.checkWalls();
+        this.vector.y *= this.fric;
+        this.vector.x *= this.fric;
     }
 
-    
     update() {
+        this.velocityChecks();
+        this.gravity();
 
-        this.velocityChecks()
+        this.x += this.vector.x;
+        this.y += this.vector.y;
 
-        this.x += this.vector.x
-        this.y += this.vector.y
-        
-        this.draw()
+        this.draw();
     }
-
-    
 }
 
 const onKeyPress = (e) => {
-    e.preventDefault()
-    if(e.keyCode === 39) {
-        circle.accel.x = .3
-        
+    e.preventDefault();
+    if (e.keyCode === 39) {
+        circle.accel.x = 0.3;
     }
-    if(e.keyCode === 37) {
-        circle.accel.x = -.3
+    if (e.keyCode === 37) {
+        circle.accel.x = -0.3;
     }
-    if(e.keyCode == 38) {
-        circle.accel.y = -.3
+    if (e.keyCode == 38) {
+        circle.accel.y = -0.3;
     }
-    if(e.keyCode == 40) {
-        circle.accel.y = .3
+    if (e.keyCode == 40) {
+        circle.accel.y = 0.3;
     }
-}
+};
 
 const onKeyUp = (e) => {
-    e.preventDefault()
-    if(e.keyCode == 39) {
-        circle.accel.x -= .3
+    e.preventDefault();
+    if (e.keyCode == 39) {
+        circle.accel.x -= 0.3;
     }
-    if(e.keyCode == 37) {
-        circle.accel.x += .3
+    if (e.keyCode == 37) {
+        circle.accel.x += 0.3;
     }
-    if(e.keyCode == 38) {
-        circle.accel.y += .3
+    if (e.keyCode == 38) {
+        circle.accel.y += 0.3;
     }
-    if(e.keyCode == 40) {
-        circle.accel.y -= .3
+    if (e.keyCode == 40) {
+        circle.accel.y -= 0.3;
     }
-}
+};
 
-
-let circle = new Circle(cx, cy, { x: 0, y: 0 }, 5, 'yellow')
+let circle = new Circle(cx, cy, { x: 0, y: 0 }, 5, 'yellow');
 
 const animate = () => {
+    requestAnimationFrame(animate);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
-    requestAnimationFrame(animate)
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.2)'
-    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
+    window.onkeydown = onKeyPress;
+    window.onkeyup = onKeyUp;
 
-    window.onkeydown = onKeyPress
-    window.onkeyup = onKeyUp
-    
-    circle.update()
+    circle.update();
+};
 
-    
-}
-
-animate()
-
-
-
-
-
-
-
+animate();
 
 //const circleArray = []
 
@@ -174,9 +146,8 @@ animate()
 //     let randDX = (Math.random() - 0.5) * 6
 //     let randColor = colors[Math.floor(Math.random() * (colors.length - 1))]
 
-//     circleArray.push(new Circle(randX, randY, randDX, randDY, radius, randColor))        
+//     circleArray.push(new Circle(randX, randY, randDX, randDY, radius, randColor))
 // }
-
 
 // class Triangle {
 //     constructor () {
@@ -188,7 +159,7 @@ animate()
 //         this.y2 = this.center.y + 30;
 //         this.x3 = this.center.x + 15;
 //         this.y3 = this.center.y + 30;
-       
+
 //     }
 
 //     // -10px from mouse pos
@@ -212,7 +183,6 @@ animate()
 //         this.y2 = this.center.y + 30;
 //         this.x3 = this.center.x + 15;
 //         this.y3 = this.center.y + 30;
-
 
 //         this.draw()
 //     }
